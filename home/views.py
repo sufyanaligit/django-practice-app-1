7c98450fa6759f6f82243db95982a6fd36c65b84
sufyanaligit <informsufyan@gmail.com>
2023-06-17 21:39:18 +0500

    intial commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('This is homepage')
+
+def about(request):
+    return HttpResponse('This is about')  
+
+def services(request):
+    return HttpResponse('This is services')  
+
+def contact(request):
+    return HttpResponse('This is contact')            
