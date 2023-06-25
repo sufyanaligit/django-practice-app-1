5b7943b8ef7a08aaf1f1fe8d2021cc6a97fc557e
sufyanaligit <informsufyan@gmail.com>
2023-06-25 21:44:46 +0500

    hello app changes

--- Hello/settings.py
+++ Hello/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Harry Ice Cream Admin"
+admin.site.site_title = "Harry Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Harry Ice Creams"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
