357384ff6bba50dbf642a5315f4c05158f711c70
sufyanaligit <informsufyan@gmail.com>
2023-06-27 22:18:25 +0500

    minor fix

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Harry Ice Cream Admin"
-admin.site.site_title = "Harry Ice Cream Admin Portal"
-admin.site.index_title = "Welcome to Harry Ice Creams"
+admin.site.site_header = "Sufyan Ice Cream Admin"
+admin.site.site_title = "Sufyan Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Sufyan Ice Creams"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
